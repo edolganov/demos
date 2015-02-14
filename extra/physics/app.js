7e73c3278dff3da9a1d45743006e2dd5ce2d3919
edolganov <evgenij.dolganov@gmail.com>
2015-02-15 01:31:56 +0300

    _

--- extra/physics/app.js
+++ extra/physics/app.js
@@ -10,17 +10,44 @@ function App(){
 	var w = canvas.width();
 	var h = canvas.height();
 	var intervalId;
+	var delta = 15;
+	var x, y;
+
 
 
 	this.init = function(){
 
-		intervalId = setInterval(updateWorld, 100);
-
+		//intervalId = setInterval(updateWorld, 100);
+		updateWorld();
 
 	};
 
 	function updateWorld(){
+
 		c.fillRect(0, 0, w, h);
+
+		c.strokeStyle = 'white';
+		c.lineWidth="1";
+
+		for(y = delta; y <= h; y += delta){
+			for(x = delta; x <= w; x += delta){
+				c.beginPath();
+				c.moveTo(x - delta, y);
+				c.lineTo(x, y);
+				c.stroke();
+			}
+		}
+
+		for(x = delta; x <= w; x += delta){
+			for(y = delta; y <= h; y += delta){
+				c.beginPath();
+				c.moveTo(x, y - delta);
+				c.lineTo(x, y);
+				c.stroke();
+			}
+		}
+
+
 	}
 
 }
